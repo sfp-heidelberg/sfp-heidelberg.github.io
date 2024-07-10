4bbbc6b61e2530086f95f485abce028116c7606b
sfp-heidelberg <sfp_heidelberg@protonmail.com>
2024-07-10 15:08:24 +0200

    add page transition & favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	import Header from '$lib/components/header.svelte'
 	import Footer from '$lib/components/footer.svelte'
+	import Transition from '$lib/components/transition.svelte'
 	import 'open-props/style'
 	import 'open-props/normalize'
 	import 'open-props/buttons'
 	import '../app.css'
+
+	export let data
 </script>
 
 <div class="layout">
 	<Header />
 	<main>
-		<slot />
+		<Transition url={data.url}>
+			<slot />
+		</Transition>
+		
 	</main>
 
 	<Footer />
